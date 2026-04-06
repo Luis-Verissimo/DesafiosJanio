8b3e446aaade21b4e9fd210da452afd837141522
Luis Gabriel W Verissimo Leite <luis.leite@infomedbenner.com.br>
2026-04-06 13:21:21 -0300

    Separating methods from the main class

---
+++ Desafio1/Desafio1/Metodos.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using System.Threading;
+using System;
+
+namespace desafio1;
+
+public class Metodos
+{
+    public static void InserirNome()
+    {
+        Console.WriteLine("Qual o nome você deseja colocar no arquivo?");
+        string nome = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(nome))
+        {
+            Console.WriteLine("O sobrenome que você deu é invalido ou nullo!");
+            Thread.Sleep(900);
+            Console.Clear();
+            InserirNome();
+        }
+        else
+        {
+
+            string nomeF = char.ToUpper(nome[0]) + nome.Substring(1).ToLower();
+
+            string caminhoArquivo = $@"E:\Desenvolvimento\Desafio1\nome.txt";
+            File.AppendAllText(caminhoArquivo, $"{nomeF}" + Environment.NewLine);
+        }
+    }
+
+    public static void InserirSobrenome()
+    {
+        Console.WriteLine("Qual o sobrenome você deseja colocar no arquivo?");
+        string sobrenome = Console.ReadLine();
+        if (string.IsNullOrEmpty(sobrenome))
+        {
+            Console.WriteLine("O sobrenome que você deu é invalido ou nullo!");
+            Thread.Sleep(900);
+            Console.Clear();
+            InserirSobrenome();
+        }
+        else
+        {
+
+            string sobrenomeF = char.ToUpper(sobrenome[0]) + sobrenome.Substring(1).ToLower();
+
+            string caminhoArquivo = $@"E:\Desenvolvimento\Desafio1\sobrenome.txt";
+            File.AppendAllText(caminhoArquivo, $"{sobrenomeF}" + Environment.NewLine);
+        }
+    }
+
+    public static void DeletarNomeSobrenome()
+    {
+        Console.WriteLine("O que você quer deletar?");
+        Console.WriteLine("1) - Nome");
+        Console.WriteLine("2) - Sobrenome");
+        string opNomeSobrenome = Console.ReadLine();
+
+        //if (opnomesobrenome == 1)
+        //{
+
+        //}
+        //else if (opnomesobrenome == 2)
+        //{
+
+        //}
+        //else
+        //{
+        //    console.writeline("operação invalida");
+        //    thread.sleep(800);
+        //    console.clear();
+        //    thread.sleep(800);
+        //    deletarnomesobrenome();
+        //}
+    }
+    public static void GerarNomeAleatorio()
+    {
+
+    }
+}
--- Desafio1/Desafio1/Program.cs
+++ Desafio1/Desafio1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Threading;
+using System;
 
 namespace desafio1;
 
@@ -7,7 +8,7 @@ class Program
 {
     public static void Main()
     {
-        Menu();
+        Menu(); 
     }
 
     public static void Menu()
@@ -36,84 +37,12 @@ class Program
         
         switch (op)
         {
-           case 1: Console.Clear(); InserirNome(); break;
-           case 2: Console.Clear(); InserirSobrenome(); break;
-           case 3: Console.Clear(); DeletarNomeSobrenome(); break;
-           case 4: Console.Clear(); GerarNomeAleatorio();  break;
+           case 1: Console.Clear(); Metodos.InserirNome(); break;
+           case 2: Console.Clear(); Metodos.InserirSobrenome(); break;
+           case 3: Console.Clear(); Metodos.DeletarNomeSobrenome(); break;
+           case 4: Console.Clear(); Metodos.GerarNomeAleatorio();  break;
            case 5: return; break;
         }
         
-    }
-
-    public static void InserirNome() 
-    {
-        Console.WriteLine("Qual o nome você deseja colocar no arquivo?");
-        string nome = Console.ReadLine();
-
-        if (string.IsNullOrEmpty(nome))
-        {
-            Console.WriteLine("O sobrenome que você deu é invalido ou nullo!");
-            Thread.Sleep(900);
-            Console.Clear();
-            InserirNome();
-        }
-        else
-        {
-
-            string nomeF = char.ToUpper(nome[0]) + nome.Substring(1).ToLower();
-
-            string caminhoArquivo = $@"E:\Desenvolvimento\Desafio1\nome.txt";
-            File.AppendAllText(caminhoArquivo, $"{nomeF}" + Environment.NewLine);
-        }
-    }
-
-    public static void InserirSobrenome() 
-    {
-        Console.WriteLine("Qual o sobrenome você deseja colocar no arquivo?");
-        string sobrenome = Console.ReadLine();
-        if (string.IsNullOrEmpty(sobrenome))
-        {
-            Console.WriteLine("O sobrenome que você deu é invalido ou nullo!");
-            Thread.Sleep(900);
-            Console.Clear();
-            InserirSobrenome();
-        }
-        else
-        {
-
-            string sobrenomeF = char.ToUpper(sobrenome[0]) + sobrenome.Substring(1).ToLower();
-
-            string caminhoArquivo = $@"E:\Desenvolvimento\Desafio1\sobrenome.txt";
-            File.AppendAllText(caminhoArquivo, $"{sobrenomeF}" + Environment.NewLine);
-        }
-    }
-
-    public static void DeletarNomeSobrenome()
-    {
-        Console.WriteLine("O que você quer deletar?");
-        Console.WriteLine("1) - Nome");
-        Console.WriteLine("2) - Sobrenome");
-        string opNomeSobrenome = Console.ReadLine();
-
-        //if (opNomeSobrenome == 1)
-        //{
-
-        //}
-        //else if (opNomeSobrenome == 2)
-        //{
-
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Operação invalida");
-        //    Thread.Sleep(800);
-        //    Console.Clear();
-        //    Thread.Sleep(800);
-        //    DeletarNomeSobrenome();
-        //}
-    }
-    public static void GerarNomeAleatorio() 
-    {
-
     }
 }
